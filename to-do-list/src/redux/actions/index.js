a686a7c18c026d81f0ea825e6a2c46cdb4026cc7
jessi68 <lynn312@unist.ac.kr>
2021-08-20 21:28:23 +0900

    feat: action 에서 payload 속성 추가함

--- to-do-list/src/redux/actions/index.js
+++ to-do-list/src/redux/actions/index.js
@@ -2,20 +2,23 @@ export const ADD_TO_DO = "add_to_do";
 export const DELETE_TO_DO = "delete_to_do";
 export const COMPLETE_TO_DO ="complete_to_do";
 
-export function add() {
+export function add(todo) {
     return {
-        type: ADD_TO_DO
+        type: ADD_TO_DO,
+        payload: todo
     };
 }
 
-export function remove() {
+export function remove(todo) {
     return {
-        type: DELETE_TO_DO
+        type: DELETE_TO_DO,
+        payload: todo
     };
 }
 
-export function complete() {
+export function complete(todo) {
     return {
-        type: COMPLETE_TO_DO;
-    }
+        type: COMPLETE_TO_DO,
+        payload: todo
+    };
 }
