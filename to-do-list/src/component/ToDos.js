7fae0658f0502ee94a9b4a621572be84fd61bba5
jessi68 <lynn312@unist.ac.kr>
2021-08-20 22:04:22 +0900

    feat: toDo component 여러개 한꺼번에 만드는 기능 구현

--- to-do-list/src/component/ToDos.js
+++ to-do-list/src/component/ToDos.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ToDo from "./ToDo";
 
 
 export default class ToDos extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.toToContents = useSelector(store => store.toDoReducer);
     }
 
     render() {
+        const toDoViews = this.toDoContents.map((todo, idx) => {
+            return <ToDo text={todo}></ToDo>
+        });
 
+        <section>
+            {toDoViews}
+        </section>
     }
 }
